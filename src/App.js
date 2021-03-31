6142a582256dfef61511edd2ee79b10285469a4c
LEARN Academy <>
2021-03-31 10:50:23 -0700

    added read functionality

--- src/App.js
+++ src/App.js
@@ -10,21 +10,36 @@ import NotFound from "./pages/NotFound";
 import cats from "./mockCats.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+
+
 class App extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			cats: cats
+		}
+	}
+
 	render() {
-		return (
-			<Router>
-				<Header />
-				<Switch>
-					<Route exact path='/' component={Home} />
-					<Route path='/catindex' component={CatIndex} />
-					<Route path='/catshow' component={CatShow} />
-					<Route path='/catnew' component={CatNew} />
-					<Route path='/catedit' component={CatEdit} />
-					<Route component={NotFound} />
-				</Switch>
-				<div class='footer'>footer</div>
-			</Router>
+    return (
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route
+            path="/catindex"
+            render = { () => <CatIndex cats={this.state.cats}/> }/>
+          <Route path="/catshow/:id" render = {(props) => {
+            const id = +props.match.params.id;
+            const foundKitty = this.state.cats.find(cat => cat.id === id);
+            return <CatShow cat={foundKitty}/>}
+          } />
+          <Route path="/catnew" component = { CatNew } />
+          <Route path="/catedit" component = { CatEdit } />
+          <Route component = { NotFound } />
+        </Switch>
+        <Footer />
+      </Router>
 		);
 	}
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 
 class Home extends Component {
 	render() {
-		return <>Home</>;
+		return (
+			<>Home
+			</>
+		);
 	}
 }
 export default Home;
